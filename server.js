d720ea187ac24a0bbbb3bbeffa1941dd97857e16
Chetan Kantharia <chetan.kantharia@gmail.com>
2018-03-05 00:37:58 +0530

    Mutation

--- server.js
+++ server.js
@@ -4,40 +4,78 @@ var { buildSchema } = require("graphql");
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
-  type RandomDie {
-    numSides: Int!
-    rollOnce: Int!
-    roll(numRolls: Int!): [Int]
+  input MessageInput {
+    content: String
+    author: String
+  }
+
+  type Message {
+    id: ID!
+    content: String
+    author: String
   }
 
   type Query {
-    getDie(numSides: Int): RandomDie
+    getMessage(id: ID!): Message
+    getMessages: [Message]
+  }
+
+  type Mutation {
+    createMessage(input: MessageInput): Message
+    updateMessage(id: ID!, input: MessageInput): Message
   }
 `);
 
-// This class implements the RandomDie GraphQL type
-class RandomDie {
-  constructor(numSides) {
-    this.numSides = numSides;
-  }
-
-  rollOnce() {
-    return 1 + Math.floor(Math.random() * this.numSides);
-  }
-
-  roll({ numRolls }) {
-    var output = [];
-    for (var i = 0; i < numRolls; i++) {
-      output.push(this.rollOnce());
-    }
-    return output;
+// If Message had any complex fields, we'd put them on this object.
+class Message {
+  constructor(id, { content, author }) {
+    this.id = id;
+    this.content = content;
+    this.author = author;
   }
 }
 
-// The root provides the top-level API endpoints
+// Maps username to content
+var fakeDatabase = {};
+
 var root = {
-  getDie: function({ numSides }) {
-    return new RandomDie(numSides || 6);
+  getMessage: function({ id }) {
+    if (!fakeDatabase[id]) {
+      throw new Error("no message exists with id " + id);
+    }
+    return new Message(id, fakeDatabase[id]);
+  },
+  getMessages: () => {
+    const _keys = Object.keys(fakeDatabase);
+    const dataArray = [];
+
+    _keys.forEach(key => {
+      const dataSet = fakeDatabase[key];
+      dataArray.push({
+        id: key,
+        content: dataSet.content,
+        author: dataSet.author
+      });
+    });
+
+    return dataArray;
+  },
+  createMessage: function({ input }) {
+    // Create a random id for our "database".
+    var id = require("crypto")
+      .randomBytes(10)
+      .toString("hex");
+
+    fakeDatabase[id] = input;
+    return new Message(id, input);
+  },
+  updateMessage: function({ id, input }) {
+    if (!fakeDatabase[id]) {
+      throw new Error("no message exists with id " + id);
+    }
+    // This replaces all old data, but some apps might want partial update.
+    fakeDatabase[id] = input;
+    return new Message(id, input);
   }
 };
 
@@ -50,5 +88,6 @@ app.use(
     graphiql: true
   })
 );
-app.listen(4000);
-console.log("Running a GraphQL API server at localhost:4000/graphql");
+app.listen(4000, () => {
+  console.log("Running a GraphQL API server at localhost:4000/graphql");
+});
